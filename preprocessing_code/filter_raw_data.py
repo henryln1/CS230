2327e70d86ab7a2679a94f5e0a8d35ea29f1fbe0
henryln1 <henritienne@gmail.com>
2019-05-04 20:14:00 -0700

    finished minibatch script

--- preprocessing_code/filter_raw_data.py
+++ preprocessing_code/filter_raw_data.py
@@ -1,5 +1,55 @@
-import pandas
+import pandas as pd
+import sys
+import lzma
+import json
 
 '''
 Process raw data files from pushshift.io and extract only what we need.
 '''
+
+subreddits = ['AskReddit']
+
+submissions_list = []
+
+def process_file(file_name):
+	with lzma.open(file_name) as file:
+		counter = 0
+		for line in file:
+			json_format_line = json.loads(line)
+			subreddit = json_format_line['subreddit']
+			if subreddit in subreddits:
+				score = json_format_line['score']
+				title = json_format_line['title']
+				title = title.replace(',', '') #should replace with a whole preprocessing function later
+				number_comments = json_format_line['num_comments']
+				timestamp = json_format_line['created_utc']
+				submissions_list.append((subreddit, title, score, number_comments, timestamp)) #append this to list
+			counter += 1
+			if counter % 100000 == 0:
+				print(counter, " lines processed.")
+
+def write_to_csv(file_name):
+	print("Writing to CSV located at " + file_name)
+	column_names = ['Subreddit', 'Title', 'Score', 'Num_Comments', 'Timestampe']
+	df = pd.DataFrame(submissions_list, columns = column_names)
+	df.to_csv(file_name)
+
+
+def main():
+	print("Beginning to process .xz file..")
+	file_directory = '../../project_data/'
+	file_name = 'RS_2018-09.xz'
+	csv_directory = file_directory + 'csv_files/'
+	csv_file_name = csv_directory + file_name[:-3] + '_' + subreddits[0] + '_submissions.csv'
+	print("Csv file name: ", csv_file_name)
+	process_file(file_directory + file_name)
+	write_to_csv(csv_file_name)
+	print("Done writing to csv file. Exiting.")
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
---
+++ preprocessing_code/generate_minibatch.py
@@ -0,0 +1,67 @@
+import sys
+import numpy as np
+import random
+import re
+import csv
+
+EXAMPLE_LENGTH = 50
+
+
+
+
+
+
+def build_minibatch(minibatch_size, glove_vectors, data_file_location):
+	glove_vector_dim = len(glove_vectors['the'])
+	X = np.zeros(shape = (minibatch_size, EXAMPLE_LENGTH, glove_vector_dim))
+	Y = np.zeros(shape = (minibatch_size, 1))
+	num_data_points = num_lines = sum(1 for line in open(data_file_location))
+	random_rows = [random.randint(1, num_data_points) for x in range(minibatch_size)]
+	data_points = []
+	with open(data_file_location) as fd:
+		reader = csv.reader(fd)
+		data_points = [row for idx, row in enumerate(reader) if idx in random_rows]
+	print(data_points[0])
+	for i in range(len(data_points)):
+		_, subreddit, title, score, num_comments, timestamp = tuple(data_points[i])
+		print("Title: ", title)
+		title_words_list = re.sub(r'[^a-zA-Z ]', '', title).split()
+		title_words_list = [x.lower() for x in title_words_list]
+		print("Words: ", title_words_list)
+		for j in range(len(title_words_list)):
+			curr_word = title_words_list[j]
+			if curr_word in glove_vectors:
+				X[i][j] = glove_vectors[curr_word]
+		print("X: ", X[i])
+		Y[i] = score
+		print("Y: ", Y[i])
+	return X, Y
+
+
+
+def build_glove_dict(file_name):
+	glove_dict = {}
+	with open(file_name, 'r') as f:
+		for line in f:
+			line_list = line.split(" ")
+			word = line_list[0]
+			embeddings = line_list[1:]
+			assert(str(len(embeddings)) in file_name) #making sure dimensions match file name
+			embeddings = [float(x) for x in embeddings]
+			glove_dict[word] = embeddings
+	f.close()
+	return glove_dict
+
+
+def main():
+	assert len(sys.argv) == 3 #make sure we are given a glove vector dim and minibatch size
+	glove_vector_dimensions = sys.argv[1]
+	minibatch_size = int(sys.argv[2])
+	data_file_location = '../../project_data/csv_files/RS_2018-09_AskReddit_submissions.csv'
+	glove_vector_location = '../glove.6B/glove.6B.' + str(glove_vector_dimensions) + 'd.txt'
+	glove_vectors_dict = build_glove_dict(glove_vector_location)
+	X, Y = build_minibatch(minibatch_size, glove_vectors_dict, data_file_location)
+
+
+if __name__ == "__main__":
+	main()
