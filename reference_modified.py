9a0e376da9c46de549994803c501003f63041540
henryln1 <henritienne@gmail.com>
2019-05-14 00:05:27 -0700

    running model

--- reference_modified.py
+++ reference_modified.py
@@ -10,6 +10,8 @@ import time
 import csv
 import numpy as np
 import re
+import random
+from sklearn.metrics import mean_squared_error
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,15 +24,17 @@ input_size = 50
 hidden_size = 128
 num_layers = 2
 num_classes = 10
-batch_size = 4096
+batch_size = 512
 num_epochs = 4
 learning_rate = 0.0001
-dev_batch_size = 1024
+dev_batch_size = 512
 
 EXAMPLE_LENGTH = 100
 
 glove_vectors = generate_minibatch.export_glove(input_size)
 glove_vector_dim = len(glove_vectors['the'])
+
+
 
 # Read in train data
 train_images = None
@@ -134,7 +138,7 @@ def count_parameters(model):
 
 model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
 number_params = count_parameters(model)
-print("Number of trainable Parameters: ", number_params)
+print("Number of trainable parameters: ", number_params)
 
 # Loss and optimizer
 # criterion = nn.CrossEntropyLoss()
@@ -142,11 +146,24 @@ criterion = nn.MSELoss()
 # Use mean squared error
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
+# checks weights are updating part 1
+# old_state_dict = {}
+# for key in model.state_dict():
+#     old_state_dict[key] = model.state_dict()[key].clone()
+
+
+dev_error_history = 'dev_model_1_error_history.txt'
+train_error_history = 'train_model_1_error_history.txt'
+
+train_f = open(train_error_history, "a+")
+dev_f = open(dev_error_history, "a+")
+
 # Train the model
 total_step = len(train_labels)
 for epoch in range(num_epochs):
     start_time = time.time()
     for i in range(len(train_images)):
+    # for i in range(100): #training on small set
     # for i, (images, labels) in enumerate(train_loader):
         images = train_images[i].reshape(-1, sequence_length, input_size).type('torch.FloatTensor').to(device)
         labels = train_labels[i].type('torch.FloatTensor').to(device)
@@ -164,23 +181,114 @@ for epoch in range(num_epochs):
         if (i+1) % 100 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+            # checks weights are updating part 2
+            # new_state_dict = {}
+            # for key in model.state_dict():
+            #     new_state_dict[key] = model.state_dict()[key].clone()
+            # for key in old_state_dict:
+            #     if not (old_state_dict[key] == new_state_dict[key]).all():
+            #         print('Diff in {}'.format(key)
+
+        if (i+1) % 1000 == 0:
+            print('Evaluating on last 500 examples from training set.')
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                predicted_list = []
+                actual_label_list = []
+                for j in range(i, i - 500, -1):
+                    images = dev_images[j].reshape(-1, sequence_length, input_size).type('torch.FloatTensor').to(device)
+                    labels = dev_labels[j].type('torch.FloatTensor').to(device)
+                    outputs = model(images)
+                    predicted, _ = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                    actual_label_list.append(dev_labels[j])
+                    predicted_list.append(predicted.item())
+                print('Train Accuracy of the model on the 500 training examples: {} %'.format(100 * correct / total))               
+                print('Mean Squared Error: ', mean_squared_error(predicted_list, actual_label_list))
+                train_f.write("Predicted: " + str(predicted_list) + '\n')
+                train_f.write("Actual: " + str(actual_label_list) + '\n')
+                train_f.write("MSE: " + str(mean_squared_error(predicted_list, actual_label_list)) + '\n')
+
+            print('Next, evaluating on 500 examples from dev set.')
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                random_start = random.randint(1, len(dev_labels) - 1000)
+                predicted_list = []
+                actual_label_list = []
+                for j in range(random_start, random_start + 500):
+                    images = dev_images[j].reshape(-1, sequence_length, input_size).type('torch.FloatTensor').to(device)
+                    labels = dev_labels[j].type('torch.FloatTensor').to(device)
+                    outputs = model(images)
+                    predicted, _ = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                    actual_label_list.append(dev_labels[j])
+                    predicted_list.append(predicted.item())
+                print('Dev Accuracy of the model on the 500 dev examples: {} %'.format(100 * correct / total)) 
+                print('Mean Squared Error: ', mean_squared_error(predicted_list, actual_label_list))
+                dev_f.write("Predicted: " + str(predicted_list) + '\n')
+                dev_f.write("Actual: " + str(actual_label_list) + '\n')
+                dev_f.write("MSE: " + str(mean_squared_error(predicted_list, actual_label_list)) + '\n')
+
     time_elapsed = time.time() - start_time
     print("Epoch: " , str(epoch + 1), " took ", str(time_elapsed), " seconds.")
 
+
+dev_output_file = 'dev_model_outputs_1.txt'
+train_output_file = 'train_model_outputs_1.txt'
 # Test the model
 with torch.no_grad():
     correct = 0
     total = 0
+    predicted_list = []
+    actual_label_list = []
     for i in range(len(dev_labels)):
     # for images, labels in test_loader:
         images = dev_images[i].reshape(-1, sequence_length, input_size).type('torch.FloatTensor').to(device)
-        labels = dev_labels[i].type('torch.LongTensor').to(device)
+        labels = dev_labels[i].type('torch.FloatTensor').to(device)
         outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
+        predicted, _ = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        actual_label_list.append(dev_labels[i])
+        predicted_list.append(predicted.item())
+    print('Dev Accuracy of the model on the full dev set: {} %'.format(100 * correct / total)) 
+    print('Mean Squared Error: ', mean_squared_error(predicted_list, actual_label_list))
 
-    print('Dev Accuracy of the model on the 1024 dev examples: {} %'.format(100 * correct / total)) 
+print("Predicted: ", predicted_list)
+print("Actual: ", actual_label_list)
+f = open(dev_output_file, "a+")
+f.write('Predicted: ' + str(predicted_list) + '\n')
+f.write('Actual: ' + str(actual_label_list) + '\n')
+
+
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    predicted_list = []
+    actual_label_list = []
+    for i in range(len(train_labels)):
+    # for images, labels in test_loader:
+        images = train_images[i].reshape(-1, sequence_length, input_size).type('torch.FloatTensor').to(device)
+        labels = train_labels[i].type('torch.FloatTensor').to(device)
+        outputs = model(images)
+        predicted, _ = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        actual_label_list.append(train_labels[i])
+        predicted_list.append(predicted.item())
+    print('Dev Accuracy of the model on the full dev set: {} %'.format(100 * correct / total)) 
+    print('Mean Squared Error: ', mean_squared_error(predicted_list, actual_label_list))
+    
+print("Predicted: ", predicted_list)
+print("Actual: ", actual_label_list)
+f = open(train_output_file, "a+")
+f.write('Predicted: ' + str(predicted_list) + '\n')
+f.write('Actual: ' + str(actual_label_list) + '\n')
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
