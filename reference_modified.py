47abb2c8931788d65b2e38bd41aea7bd8fa76da9
henryln1 <henritienne@gmail.com>
2019-06-03 12:24:11 -0700

    more fixes

--- reference_modified.py
+++ reference_modified.py
@@ -118,7 +118,7 @@ for epoch in range(num_epochs):
 		num_correct += torch.sum(idxs == torch.round(train_y.type('torch.FloatTensor')).to(device)).item()
 		# print("Predictions: {}".format(idxs))
 		# print("Actual: {}".format(train_y))
-		counter += 1:
+		counter += 1
 		if counter % 25 == 0:
 			print("Loss: {}".format(loss) )
 		train_losses.append(loss.item())
