f1e3bbbc4b34904e58d701a0aae6d0d75c4cdddc
henryln1 <henritienne@gmail.com>
2019-06-07 17:12:50 -0700

    variables renamed

--- test_models.py
+++ test_models.py
@@ -14,20 +14,56 @@ import numpy as np
 import util as U
 import constants as C
 
-from reference_modified import BiRNN
-from classification_model import ClassBiRNN
 
 input_size = 50
 hidden_size = 1024
 num_layers = 2
 BATCH_SIZE = 64
 
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+# sequence_length = 100
+input_size = 50
+hidden_size = 1024
+num_layers = 2
+num_epochs = 10
+num_classes = 20
+learning_rate = 0.00001
+
+BATCH_SIZE = 64
+
+class ClassBiRNN(nn.Module):
+	def __init__(self, glove_vec, input_size, hidden_size, num_layers, num_classes):
+		super(ClassBiRNN, self).__init__()
+		self.hidden_size = hidden_size
+		self.num_layers = num_layers
+		self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+		self.fc = nn.Linear(hidden_size*2, num_classes)  # 2 for bidirection
+		self.glove = glove_vec
+		self.embed = nn.Embedding.from_pretrained(torch.tensor(glove_vec.vectors))
+	
+	def forward(self, x):
+		# Set initial states
+		print("forward")
+		embedding = self.embed(x) # B x L x E (batch x input_length x embedding_dim)
+		h0 = torch.zeros(self.num_layers*2, embedding.size(0), self.hidden_size).to(device) # 2 for bidirection 
+		c0 = torch.zeros(self.num_layers*2, embedding.size(0), self.hidden_size).to(device)
+		# Forward propagate LSTM
+		out, _ = self.lstm(embedding, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size*2)
+		# Decode the hidden state of the last time step
+		out = self.fc(out[:, -1, :])
+		# print(out)
+		return out
+
 # Regression model hyperparams
 r_num_classes = 1
 
 # Classification model hyperparams
 c_num_classes = 20
-
+# 
 print("Loading GloVe vectors...")
 glove = U.load_glove()
 
