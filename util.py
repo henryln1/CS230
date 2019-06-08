da9f9de02e5f877a22ed547adc8713ef2eca3edf
henryln1 <henritienne@gmail.com>
2019-06-07 20:07:45 -0700

    blah

--- util.py
+++ util.py
@@ -110,15 +110,15 @@ def process_as_classification(label):
 	@param label (int): Reddit score
 	@return: classification label
 	"""
-	print("Label: ", label)
+	# print("Label: ", label)
 	# upper_bounds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 	upper_bounds = [0, 1, 5, 10, 20, 30, 40, 50, 60, 70, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 	for idx in range(1, len(upper_bounds)):
 		if label in range(upper_bounds[idx - 1], upper_bounds[idx]):
-			print("IDX: ", idx - 1)
+			# print("IDX: ", idx - 1)
 			return idx - 1
 	if label >= 1000:
-		print("IDX: ", len(upper_bounds) - 1)
+		# print("IDX: ", len(upper_bounds) - 1)
 		return len(upper_bounds) - 1
 
 
